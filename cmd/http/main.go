b81eb41edfb96651c0e85a85274743fcff8a5fb5
trainee.p.kurakin <pasanoon1507@gmail.com>
2025-05-20 10:14:13 +0400

    commitik

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -13,11 +13,11 @@ import (
 // main - инициализирует хранилище, сервисный слой и регистрирует HTTP-маршруты.
 func main() {
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
 	)
 	storage := postgres.NewStoragepostgres(dsn)
